93128f3bfcc8e013a41524b48da890a5fa7c3f65
Kamil Chwedura <kchwedura@gmail.com>
2024-10-11 16:58:54 +0100

    Window Created
    Slow progress but been swamped with work
    
    - Fixed the CMake file
    Add the SFML folder to the project folder
    - Created a blank window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
-#include <SFML-2.6.1/Graphics.hpp>
-
+#include <SFML/Graphics.hpp>
 
 int main() {
-    
+    // Create a window object with the size of 800x600 and a name "Test Window"
+    sf::Window window(sf::VideoMode(800, 600), "Test Window");
+
+    // While the window is open, keep the window open
+    while(window.isOpen()){
+        // Create an event listener
+        sf::Event event;
+        // While window is listening to an event
+        while(window.pollEvent(event)){
+            // If the event type is equal to closed
+            if(event.type == sf::Event::Closed){
+                // Close the window
+                window.close();
+            }
+        }
+    }
+
     return 0;
 }
